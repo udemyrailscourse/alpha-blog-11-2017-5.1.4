65e196fd40876b8d92c18c9bdb993551ddf88e8b
tddror5railsudemytest2 <tddror5railsudemytest2@gmail.com>
2017-11-16 18:50:32 +0000

    Add secure password and attribute to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 105 },
             uniqueness: { case_sensitive: false },
-            format: { with: VALID_EMAIL_REGEX }  
+            format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171116161135) do
+ActiveRecord::Schema.define(version: 20171116184447) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20171116161135) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
