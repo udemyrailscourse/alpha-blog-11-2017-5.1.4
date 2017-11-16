bc86a7b4e86a77e8df5ff97d7ed5307bbefe26bb
tddror5railsudemytest2 <tddror5railsudemytest2@gmail.com>
2017-11-16 19:34:18 +0000

    Signup users from UI

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:username, :email, :password)  
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   get 'about', to: 'welcome#about'
   
   resources :articles
+  
+  get 'signup', to: "users#new"
+  resources :users, except: [:new]
 end
