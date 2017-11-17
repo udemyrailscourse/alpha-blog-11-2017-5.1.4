232976e324398ed5710c1eb1ef73fe8732173316
tddror5railsudemytest2 <tddror5railsudemytest2@gmail.com>
2017-11-17 00:03:11 +0000

    Add edit and form partial for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,20 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+  
   private
   
   def user_params
