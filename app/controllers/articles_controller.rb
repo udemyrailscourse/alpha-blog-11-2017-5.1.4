fc1a2a067cfcd2e7f0546f26edbd0ba35b21700c
tddror5railsudemytest2 <tddror5railsudemytest2@gmail.com>
2017-11-21 11:19:21 +0000

    Update all views with categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -50,7 +50,7 @@ class ArticlesController < ApplicationController
   end
   
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
 
   def require_same_user
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -20,7 +20,8 @@ class CategoriesController < ApplicationController
   end
   
   def show
-    
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
   
   private
